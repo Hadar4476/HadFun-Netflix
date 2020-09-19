d09dde667fa8edc6dd88069e9ae1d6584467093d
hadar <hadar4476@gmail.com>
2020-09-19 15:41:56 +0300

    deleted demo.php

--- templates/demo.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$password = '123456';
-$hash = password_hash($password, PASSWORD_BCRYPT);
-echo $hash . '<br>';
-$password_verify = password_verify($password, $hash);
-echo $password_verify;
